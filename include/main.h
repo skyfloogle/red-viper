a028dea2247cb62c7a2f7df2d0a09d3b2835f6ec
nop90 <nop90nop90@gmail.com>
2015-01-11 22:39:36 +0100

    Rollback to mrdanielps version

--- include/main.h
+++ include/main.h
@@ -11,13 +11,11 @@ char errmsg[256];
 void clrScreen(int screen);
 static inline void unicodeToChar(char* dst, uint16_t* src, int max);
 int romSelect(char* path);
-int LoadBitmap(char* path, u32 width, u32 height, void* buf, u32 alpha, u32 startx, u32 stride, u32 flags);
-void setKeys();
-int v810_init();
-int v810_loadRom(char * rom_name);
-void system_checkPolls();
+
+// Opens up a VB rom (pased in) and initializes the rom
+// space, some day it will initialize ram as well
+int v810_init(char * rom_name);
 
 //~ void save_sram(void);
-
 
 #endif
