c43f02279df79a5d3289a2124cdcb17d52d991b5
nop90 <nop90nop90@gmail.com>
2015-01-11 07:37:02 +0100

    Added UI for setting options

--- include/main.h
+++ include/main.h
@@ -11,13 +11,13 @@ char errmsg[256];
 void clrScreen(int screen);
 static inline void unicodeToChar(char* dst, uint16_t* src, int max);
 int romSelect(char* path);
-
-// Opens up a VB rom (pased in) and initializes the rom
-// space, some day it will initialize ram as well
-int v810_init(char * rom_name);
+int LoadBitmap(char* path, u32 width, u32 height, void* buf, u32 alpha, u32 startx, u32 stride, u32 flags);
+void setKeys();
+int v810_init();
+int v810_loadRom(char * rom_name);
+void system_checkPolls();
 
 //~ void save_sram(void);
 
-void system_checkPolls();
 
 #endif
