skyfloogle/red-viper

commit 801d7f56d4278439bd89e40ad43f754a37abfcf0

Small fixes (Wario Land and Teleroboxer boot):
 * Some immediate instructions don't sext.
 * Fix some V810->ARM conditions.
 * Make sure we check for timer interrupts.

include/arm_codegen.h
@@ -320,11 +320,14 @@
 // TODO: Implement unconditional instructions
 
 // Maps the V810 branch condition code (opcode & 0xF) to ARM conditions
+// FIXME:
+//  * BNH -> bcs + beq
+//  * BH  -> bcc + bne
 const BYTE cond_map[] = {
         // V810_OP_BV, V810_OP_BL, V810_OP_BE, V810_OP_BNH, V810_OP_BN, V810_OP_BR
-        ARM_COND_VS, ARM_COND_CC, ARM_COND_EQ, ARM_COND_LS, ARM_COND_MI, ARM_COND_AL,
+        ARM_COND_VS, ARM_COND_CS, ARM_COND_EQ, ARM_COND_LS, ARM_COND_MI, ARM_COND_AL,
         // V810_OP_BLT, V810_OP_BLE, V810_OP_BNV, V810_OP_BNL, V810_OP_BNE, V810_OP_BH,
-        ARM_COND_LT, ARM_COND_LE, ARM_COND_VC, ARM_COND_CS, ARM_COND_NE, ARM_COND_HI,
+        ARM_COND_LT, ARM_COND_LE, ARM_COND_VC, ARM_COND_CC, ARM_COND_NE, ARM_COND_HI,
         // V810_OP_BP, NOP, V810_OP_BGE, V810_OP_BGT
         ARM_COND_PL, ARM_COND_NV, ARM_COND_GE, ARM_COND_GT
 };

source/common/drc_core.c
@@ -657,26 +657,20 @@ void drc_translateBlock(exec_block *block) {
                 reg2_modified = true;
                 break;
             case V810_OP_ANDI: // andi imm16, reg1, reg2
-                MOV_I(0, (inst_cache[i].imm >> 8), 8);
-                ORR_I(0, (inst_cache[i].imm & 0xFF), 16);
-                // asr r0, r0, #16
-                new_data_proc_imm_shift(ARM_COND_AL, ARM_OP_MOV, 0, 0, 0, 16, ARM_SHIFT_ASR, 0);
+                MOV_I(0, (inst_cache[i].imm >> 8), 24);
+                ORR_I(0, (inst_cache[i].imm & 0xFF), 0);
                 ANDS(arm_reg2, arm_reg1, 0);
                 reg2_modified = true;
                 break;
             case V810_OP_XORI: // xori imm16, reg1, reg2
-                MOV_I(0, (inst_cache[i].imm >> 8), 8);
-                ORR_I(0, (inst_cache[i].imm & 0xFF), 16);
-                // asr r0, r0, #16
-                new_data_proc_imm_shift(ARM_COND_AL, ARM_OP_MOV, 0, 0, 0, 16, ARM_SHIFT_ASR, 0);
+                MOV_I(0, (inst_cache[i].imm >> 8), 24);
+                ORR_I(0, (inst_cache[i].imm & 0xFF), 0);
                 EORS(arm_reg2, arm_reg1, 0);
                 reg2_modified = true;
                 break;
             case V810_OP_ORI: // ori imm16, reg1, reg2
-                MOV_I(0, (inst_cache[i].imm >> 8), 8);
-                ORR_I(0, (inst_cache[i].imm & 0xFF), 16);
-                // asr r0, r0, #16
-                new_data_proc_imm_shift(ARM_COND_AL, ARM_OP_MOV, 0, 0, 0, 16, ARM_SHIFT_ASR, 0);
+                MOV_I(0, (inst_cache[i].imm >> 8), 24);
+                ORR_I(0, (inst_cache[i].imm & 0xFF), 0);
                 ORRS(arm_reg2, arm_reg1, 0);
                 reg2_modified = true;
                 break;
@@ -972,7 +966,7 @@ int drc_run() {
     WORD entry_PC;
 
     while (!serviceDisplayInt(clocks, v810_state->PC)) {
-//        serviceInt(clocks, PC);
+        serviceInt(clocks, PC);
 
         v810_state->PC &= V810_ROM1.highaddr;
         entry_PC = v810_state->PC;

commit 5d31dac16587f611efe331d3a2172b6e8d8fd9e2

Yet another attempt at fixing conditional branches
This fixes everything that broke with commit 801d7f5.

 * For subtraction, V810 uses the carry flag as a 'borrow' flag. When
   translating a V810 SUB or CMP operation to ARM we have to invert the
   carry flag if we want the same behaviour.
 * Reimplement BH and BNH.

include/arm_codegen.h
@@ -320,14 +320,14 @@
 // TODO: Implement unconditional instructions
 
 // Maps the V810 branch condition code (opcode & 0xF) to ARM conditions
-// FIXME:
+// NOTES:
 //  * BNH -> bcs + beq
 //  * BH  -> bcc + bne
 const BYTE cond_map[] = {
         // V810_OP_BV, V810_OP_BL, V810_OP_BE, V810_OP_BNH, V810_OP_BN, V810_OP_BR
-        ARM_COND_VS, ARM_COND_CS, ARM_COND_EQ, ARM_COND_LS, ARM_COND_MI, ARM_COND_AL,
+        ARM_COND_VS, ARM_COND_CS, ARM_COND_EQ, ARM_COND_CS, ARM_COND_MI, ARM_COND_AL,
         // V810_OP_BLT, V810_OP_BLE, V810_OP_BNV, V810_OP_BNL, V810_OP_BNE, V810_OP_BH,
-        ARM_COND_LT, ARM_COND_LE, ARM_COND_VC, ARM_COND_CC, ARM_COND_NE, ARM_COND_HI,
+        ARM_COND_LT, ARM_COND_LE, ARM_COND_VC, ARM_COND_CC, ARM_COND_NE, ARM_COND_CC,
         // V810_OP_BP, NOP, V810_OP_BGE, V810_OP_BGT
         ARM_COND_PL, ARM_COND_NV, ARM_COND_GE, ARM_COND_GT
 };

include/arm_emit.h
@@ -383,6 +383,12 @@ static inline void new_branch_link(BYTE cond, BYTE l, WORD imm) {
 #define ORR_I(Rd, imm8, rot) \
     new_data_proc_imm(ARM_COND_AL, ARM_OP_ORR, 0, Rd, Rd, rot>>1, imm8)
 
+// eor Rd, imm, ror #rot
+// Xor immediate
+// imm8 can be rotated an even number of times
+#define EOR_I(Rd, imm8, rot) \
+    new_data_proc_imm(ARM_COND_AL, ARM_OP_EOR, 0, Rd, Rd, rot>>1, imm8)
+
 // bic Rd, imm, ror #rot
 // Bit clear immediate
 // imm8 can be rotated an even number of times
@@ -557,6 +563,9 @@ static inline void new_branch_link(BYTE cond, BYTE l, WORD imm) {
         (inst_ptr-1)->needs_branch = true; \
 }
 
+#define Boff(cond, off) \
+    new_branch_link(cond, 0, (off)-2); \
+
 // bl<cond> imm
 // Branch and link
 #define BL(cond, imm) {\
@@ -579,6 +588,13 @@ static inline void new_branch_link(BYTE cond, BYTE l, WORD imm) {
 // Move from register to CPSR
 #define MSR(Rn) \
     new_move_reg_to_cpsr(ARM_COND_AL, 0, 0b1000, 0b1111, 0, Rn);
+
+// Invert the carry flag
+#define INV_CARRY() { \
+    MRS(0); \
+    EOR_I(0, 0b10, 4); \
+    MSR(0); \
+}
 
 // Load word into register using a literal pool
 #ifdef LITERAL_POOL

source/common/drc_core.c
@@ -481,7 +481,6 @@ void drc_translateBlock(exec_block *block) {
             case V810_OP_BV:
             case V810_OP_BL:
             case V810_OP_BE:
-            case V810_OP_BNH:
             case V810_OP_BN:
             case V810_OP_BR:
             case V810_OP_BLT:
@@ -489,13 +488,26 @@ void drc_translateBlock(exec_block *block) {
             case V810_OP_BNV:
             case V810_OP_BNL:
             case V810_OP_BNE:
-            case V810_OP_BH:
             case V810_OP_BP:
             case V810_OP_BGE:
             case V810_OP_BGT:
                 arm_cond = cond_map[inst_cache[i].opcode & 0xF];
                 HANDLEINT(inst_cache[i].PC);
                 B(arm_cond, 0);
+                break;
+            // Special case: bnh and bh can't be directly translated to ARM
+            case V810_OP_BNH:
+                HANDLEINT(inst_cache[i].PC);
+                // Branch if C == 1 or Z == 1
+                B(ARM_COND_CS, 0);
+                B(ARM_COND_EQ, 0);
+                break;
+            case V810_OP_BH:
+                HANDLEINT(inst_cache[i].PC);
+                // Branch if C == 0 and Z == 0
+                Boff(ARM_COND_CS, 3);
+                Boff(ARM_COND_EQ, 2);
+                B(ARM_COND_AL, 0);
                 break;
             case V810_OP_MOVHI: // movhi imm16, reg1, reg2:
                 MOV_I(0, (inst_cache[i].imm >> 8), 8);
@@ -534,6 +546,7 @@ void drc_translateBlock(exec_block *block) {
                 break;
             case V810_OP_SUB: // sub reg1, reg2
                 SUBS(arm_reg2, arm_reg2, arm_reg1);
+                INV_CARRY();
                 reg2_modified = true;
                 break;
             case V810_OP_CMP: // cmp reg1, reg2
@@ -543,6 +556,7 @@ void drc_translateBlock(exec_block *block) {
                     RSBS_I(0, arm_reg1, 0, 0);
                 else
                     CMP(arm_reg2, arm_reg1);
+                INV_CARRY();
                 break;
             case V810_OP_SHL: // shl reg1, reg2
                 LSLS(arm_reg2, arm_reg2, arm_reg1);
@@ -648,6 +662,7 @@ void drc_translateBlock(exec_block *block) {
             case V810_OP_CMP_I: // cmp imm5, reg2
                 MOV_I(0, (sign_5(inst_cache[i].imm) & 0xFF), 8);
                 CMP_IS(arm_reg2, 0, ARM_SHIFT_ASR, 24);
+                INV_CARRY();
                 reg2_modified = true;
                 break;
             case V810_OP_SHL_I: // shl imm5, reg2