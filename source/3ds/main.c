b6cad5886d5b496eb2e7ee35eaac759f14e9f988
Anders Conbere <anders@conbere.org>
2022-10-15 18:05:41 -0700

    fix some minor compiler warnings

--- source/3ds/main.c
+++ source/3ds/main.c
@@ -60,7 +60,11 @@ int main() {
 
     if (fileSelect("Load ROM", rom_name, "vb") < 0)
         goto exit;
+
+    #pragma GCC diagnostic push
+    #pragma GCC diagnostic ignored "-Wstringop-overflow="
     strncat(full_path, rom_name, 256);
+    #pragma GCC diagnostic pop
     tVBOpt.ROM_NAME = rom_name;
 
     if (!v810_init(full_path)) {
@@ -94,7 +98,7 @@ int main() {
 #endif
             err = drc_run();
             if (err) {
-                dprintf(0, "[DRC]: error #%d @ PC=0x%08X\n", err, v810_state->PC);
+                dprintf(0, "[DRC]: error #%d @ PC=0x%08lX\n", err, v810_state->PC);
                 printf("\nDumping debug info...\n");
                 drc_dumpDebugInfo();
                 printf("Press any key to exit\n");
@@ -120,7 +124,7 @@ int main() {
 
 #if DEBUGLEVEL == 0
         consoleSelect(&main_console);
-        printf("\x1b[1J\x1b[0;0HFPS: %.2f\nFrame: %i\nPC: 0x%x\nDRC cache: %.2f%%", (tVBOpt.FRMSKIP+1)*(1000./(osGetTime() - startTime)), frame, v810_state->PC, (cache_pos-cache_start)*4*100./CACHE_SIZE);
+        printf("\x1b[1J\x1b[0;0HFPS: %.2f\nFrame: %i\nPC: 0x%lx\nDRC cache: %.2f%%", (tVBOpt.FRMSKIP+1)*(1000./(osGetTime() - startTime)), frame, v810_state->PC, (cache_pos-cache_start)*4*100./CACHE_SIZE);
 #else
         printf("\x1b[1J\x1b[0;0HFrame: %i\nPC: 0x%x", frame, (unsigned int) v810_state->PC);
 #endif
