55ed199a4a98ec7495ca133f1bff1abceb088476
Floogle <18466542+skyfloogle@users.noreply.github.com>
2023-06-24 18:22:34 +0100

    increase MAX_INST because exceeding it crashes

--- include/drc_core.h
+++ include/drc_core.h
@@ -5,7 +5,7 @@
 #include "arm_emit.h"
 
 #define CACHE_SIZE  0x100000
-#define MAX_INST    2048
+#define MAX_INST    4096
 #define ARM_CACHE_REG_START 4
 #define ARM_NUM_CACHE_REGS 6
 #define MAX_NUM_BLOCKS 4096
