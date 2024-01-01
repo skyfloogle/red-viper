93c697aeef19f4500cd643f8a6764cc836931d8c
Floogle <18466542+skyfloogle@users.noreply.github.com>
2024-01-01 11:59:53 +0100

    draw softfb by default

--- source/common/vb_set.c
+++ source/common/vb_set.c
@@ -31,7 +31,7 @@ void setDefaults(void) {
     tVBOpt.DSP2X    = 0;
     tVBOpt.DYNAREC  = 1;
     tVBOpt.FASTFORWARD = 0;
-    tVBOpt.RENDERMODE = 0;
+    tVBOpt.RENDERMODE = 1;
 
     // Default keys
 #ifdef __3DS__
