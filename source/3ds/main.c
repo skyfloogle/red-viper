65423e3f954c1f30b5174434159db2161e342ecc
Floogle <18466542+skyfloogle@users.noreply.github.com>
2024-02-02 19:43:00 +0100

    frameskip

--- source/3ds/main.c
+++ source/3ds/main.c
@@ -130,24 +130,25 @@ int main() {
             goto exit;
         }
 
-        C3D_FrameBegin(0);
+        // allow frameskip when gpu is too busy (only affects virtual bowling rn)
+        if (C3D_FrameBegin(C3D_FRAME_NONBLOCK)) {
+            guiUpdate();
 
-        guiUpdate();
+            // Display a frame, only after the right number of 'skips'
+            if(tVIPREG.tFrame == tVIPREG.FRMCYC) {
+                int alt_buf = (tVIPREG.tFrameBuffer) % 2;
+                if (tVIPREG.DPCTRL & 0x0002) {
+                    video_render(alt_buf);
+                }
+                if (tVIPREG.XPCTRL & 0x0002) {
+                    memset(V810_DISPLAY_RAM.pmemory + 0x8000 * alt_buf, 0, 0x6000);
+                    memset(V810_DISPLAY_RAM.pmemory + 0x10000 + 0x8000 * alt_buf, 0, 0x6000);
+                    tDSPCACHE.DDSPDataState[alt_buf] = CPU_CLEAR;
+                }
+            }
 
-        // Display a frame, only after the right number of 'skips'
-        if(tVIPREG.tFrame == tVIPREG.FRMCYC) {
-            int alt_buf = (tVIPREG.tFrameBuffer) % 2;
-            if (tVIPREG.DPCTRL & 0x0002) {
-                video_render(alt_buf);
-            }
-            if (tVIPREG.XPCTRL & 0x0002) {
-                memset(V810_DISPLAY_RAM.pmemory + 0x8000 * alt_buf, 0, 0x6000);
-                memset(V810_DISPLAY_RAM.pmemory + 0x10000 + 0x8000 * alt_buf, 0, 0x6000);
-                tDSPCACHE.DDSPDataState[alt_buf] = CPU_CLEAR;
-            }
+            C3D_FrameEnd(0);
         }
-
-        C3D_FrameEnd(0);
 
         // Increment frame
         frame++;
