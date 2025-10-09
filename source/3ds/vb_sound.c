662d8acb8858d784cb48e7471abca212b64da384
Floogle <18466542+skyfloogle@users.noreply.github.com>
2025-10-09 18:58:29 +0200

    fix sound in multiplayer and also a bunch of performance apparently

--- source/3ds/vb_sound.c
+++ source/3ds/vb_sound.c
@@ -92,6 +92,7 @@ static void update_buf_with_freq(int ch, int samples) {
 }
 
 void sound_update(uint32_t cycles) {
+    if (!emulating_self) return;
     int remaining_samples = (cycles - sound_state.last_cycles) / CYCLES_PER_SAMPLE;
     if (remaining_samples <= 0) return;
     sound_state.last_cycles += remaining_samples * CYCLES_PER_SAMPLE;
